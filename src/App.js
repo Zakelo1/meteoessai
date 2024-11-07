3d223ef43e37ccda079f62ee95d64df67d4e8392
Zakarya El Ouahabi <zakaryaelouahabi@ZE.local>
2024-11-07 13:41:51 +0100

    meteoessai

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from 'react'
+import axios from 'axios'
 function App() {
+ const [ville, setville] = useState("")
+ const [weatherdata, setweatherdata] = useState(null)
+ 
+const getweather = async () =>{  
+  const apikey = 'd3ff86bfafb1410d96e114546240711';
+  const apiurl = `https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${ville}&lang=fr`
+  try {
+    const reponse = await axios.get(apiurl)
+    setweatherdata(reponse.data)
+  } catch(err) {
+    alert("Ville non trouver")
+  }
+
+
+}
+const handlechange =(event) => {
+ setville(event.target.value)
+}
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Méteo API</h1>
+      <input type="text" placeholder='Ville ?' onChange={handlechange} value={ville} />
+      <button onClick={getweather}>Obtenir la température</button>
+      <ul>
+        <li>Lieu : {weatherdata?.location?.name}</li>
+        <li>La température : {weatherdata?.current?.temp_c}°C</li>
+    
+      </ul>
+   
+      </div>
   );
 }
 
